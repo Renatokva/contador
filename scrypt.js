33e05eaa1fcaf760be13a0ef2afaf175054bc0e5
Renato Kennede Vidal Alves <renatokfsa@gmail.com>
2022-05-15 12:15:49 -0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,13 @@
+
+var currentNumberWraper = document.getElementById('currentNumber');
+var currentNumber=0;
+
+function increment(){
+    currentNumber = currentNumber+1;
+    currentNumberWraper.innerHTML = currentNumber;
+}
+
+function decrement(){
+    currentNumber = currentNumber-1;
+    currentNumberWraper.innerHTML = currentNumber;
+}
